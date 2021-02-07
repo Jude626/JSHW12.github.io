f7a8ffcc51f159531279e7f7e3d30a02607a8721
Jude <jantungkuno@gmail.com>
2021-02-07 16:42:25 -0500

    update employee function created

--- update.js
+++ update.js
@@ -13,9 +13,17 @@ var connection = mysql.connection({
 
 // Creating Update employee function //
 exports.updateRole = () => {
-    view.getAllEmployees(function(employeeResults) {
-        console.log(employeeResults)
+    view.getAllEmployees(function (employeeResults) {
+        console.log(employeeResults);
         var employees = [];
-        for(var i = 0; i < employeeResults.length; i++) {
-            
-        }
+        for (var i = 0; i < employeeResults.length; i++) {
+        var fullName = {
+            name: employeeResults[i].first_name + " " + employeeResults[i].last_name,
+            value: {
+                id: employeeResults[i].emp_id,
+                firstame: employeeResults[i].first_name,
+                lastname: employeeResults[i].last_name
+            }
+        };
+        employees.push(fullName)
+    };
