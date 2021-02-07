5cf64f33df3b736ed51df10eca13c4888456fb5a
Jude <jantungkuno@gmail.com>
2021-02-07 16:13:51 -0500

    making mysql connection for add.js page

--- add.js
+++ add.js
@@ -2,3 +2,12 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+// mysql connection, copied from app.js page //
+var connection = mysql.connection({
+    host: "localhost",
+    port: 8080,
+    user: "root",
+    password: "",
+    database: "",
+});
+
