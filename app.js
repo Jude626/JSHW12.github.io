c3531b1fd563162b8b63f01bea32df2a23e5165a
Jude <jantungkuno@gmail.com>
2021-02-07 15:54:08 -0500

    create db connection for mysql

--- app.js
+++ app.js
@@ -2,3 +2,13 @@
 var inquirer = require("mysql");
 var mysql = require("mysql");
 var table = require("console.table");
+
+// Creating a database connectio nto mysql //
+var connection = mysql.connection({
+    host: "localhost",
+    port: 8080,
+    user: "root",
+    password: "",
+    database: "",
+});
+
