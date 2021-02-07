50963265a2f095867ce2fcaa5de5bfb26638d657
Jude <jantungkuno@gmail.com>
2021-02-07 17:30:33 -0500

    added roles and salaries

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (dept_name) VALUES ("On Deck"), ("Office")
+INSERT INTO company_role (title, salary, dept_id) VALUES
+--Adding roles and salaries of each position--
+("Gym Manager", 100000.00, 1),
+("Pool Manager", 80000.00, 1),
+("Swim Coach", 60000.00, 2),
+("Swim Instructor", 40000.00, 2),
+("Lifeguard", 20000.00, 2),
+("Pool Maintenance", 30000.00, 3),
+("Janitor", 20000.00, 3);
+
