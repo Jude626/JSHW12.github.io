7aba9f3fb55612c4d0d22a92540f9e40b4ddc40f
Jude <jantungkuno@gmail.com>
2021-04-10 19:18:03 -0400

    added sql connect

--- remove.js
+++ remove.js
@@ -6,3 +6,13 @@ var view = require("./view");
 const dotenv = require("dotenv");
 dotenv.config();
 
+// mysql connection, copied from app.js page //
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "company_db",
+  });
+
+  
