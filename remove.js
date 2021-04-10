617239ef5e10efd981656c92b1b25d4c5af19039
Jude <jantungkuno@gmail.com>
2021-04-10 19:18:20 -0400

    added update employee function

--- remove.js
+++ remove.js
@@ -15,4 +15,20 @@ var connection = mysql.createConnection({
     database: "company_db",
   });
 
-  
+  // Creating Update employee function //
+exports.removeEmployee = () => {
+    view.getAllEmployees(function (employeeResults) {
+      console.log(employeeResults);
+      var employees = [];
+      for (var i = 0; i < employeeResults.length; i++) {
+        var fullName = {
+          name: employeeResults[i].first_name + " " + employeeResults[i].last_name,
+          value: {
+            id: employeeResults[i].emp_id,
+            firstname: employeeResults[i].first_name,
+            lastname: employeeResults[i].last_name,
+          },
+        };
+        employees.push(fullName);
+      }
+      
