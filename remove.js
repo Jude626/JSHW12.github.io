46d0b6d97f6295a282026c809cc93e82ee9dae2f
Jude <jantungkuno@gmail.com>
2021-04-10 19:17:37 -0400

    added global variables for remove.js file

---
+++ remove.js
@@ -0,0 +1,8 @@
+// Global Variables //
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var app = require("./app");
+var view = require("./view");
+const dotenv = require("dotenv");
+dotenv.config();
+
--- view.js
+++ view.js
@@ -2,49 +2,49 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var app = require("./app");
-
+const dotenv = require("dotenv");
+dotenv.config();
 // mysql connection, copied from app.js page //
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Kay626#",
-    database: "company_db",
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "company_db",
 });
 
 // Creating view employees function //
 exports.viewAllEmployees = () => {
-    connection.queryString, function(err,res) {
-        if(error) throw error,
-        console.table(res)
-        app.start();
+  connection.query(
+    "SELECT first_name, last_name, title , salary  FROM employees Left JOIN company_role on employees.emp_role_id = company_role.role_id",
+    function (error, results) {
+      if (error) console.error(error);
+      console.table(results);
+      app.start();
     }
+  );
 };
 
 // Create function to export all roles //
-exports.getAllRoles = () => {
-    return new Promise (function(resolve, reject){
-       var results = connection.query("SELECT * FROM company_role", function(error,results) {
-        if(error) reject(error);
-        resolve(results);  
-    })
-    
-    }) 
-   
-}
+exports.getAllRoles = (callback) => {
+  var results = connection.query("SELECT * FROM company_role", function (error, results) {
+    if (error) throw error;
+    callback(results);
+  });
+};
 
 // Create function to export all departments //
 exports.getAllDepartments = () => {
-    connection.query("SELECT * FROM department", function(error,results) {
-        if(error) throw error;
-        return results;
-    })
-}
+  connection.query("SELECT * FROM department", function (error, results) {
+    if (error) throw error;
+    return results;
+  });
+};
 
 // Create function to export all employees //
-exports.getAllEmployees = () => {
-    connection.query("SELECT * FROM employees", function(err,results) {
-        if(error) throw error;
-        return results;
-    })
-}
+exports.getAllEmployees = (callback) => {
+  return connection.query("SELECT * FROM employees", function (error, results) {
+    if (error) throw error;
+    callback(results);
+  });
+};
